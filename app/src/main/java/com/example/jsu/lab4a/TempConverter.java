93c600f06d67e03bb13511d9b820bd4b534bc66f
Marcus Shell <mshell2stu.jsu.edu>
2019-03-11 07:14:53 -0500

    Layouts should be complete now

--- app/src/main/java/com/example/jsu/lab4a/TempConverter.java
+++ app/src/main/java/com/example/jsu/lab4a/TempConverter.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class TempConverter extends AppCompatActivity {
 
@@ -11,6 +12,21 @@ public class TempConverter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_converter);
+    }
+
+    public void calculateTemp(View v){
+        EditText milesText = (EditText) findViewById(R.id.Miles);
+
+        EditText kilomText = (EditText) findViewById(R.id.Kilometers);
+
+        if(milesText.getText().toString().isEmpty()){
+            if(!kilomText.getText().toString().isEmpty()){
+                double miles = Double.parseDouble(milesText.getText().toString());
+                double kilom = Double.parseDouble(kilomText.getText().toString());
+
+            }
+        }
+
     }
 
     public void backButton(View back){
--- app/src/main/java/com/example/jsu/lab4a/TipCalculator.java
+++ app/src/main/java/com/example/jsu/lab4a/TipCalculator.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.content.Intent;
+import android.widget.*;
+
+import java.text.DecimalFormat;
 
 public class TipCalculator extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class TipCalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+        ((TextView)findViewById(R.id.CalculatedTip)).setText("");
+    }
+
+    public void calculateButton(View calculate){
+
+
+        double total = Double.parseDouble(((EditText)findViewById(R.id.TotalBill)).getText().toString());
+        int bills = Integer.parseInt(((EditText)findViewById(R.id.NumberOfPeople)).getText().toString());
+        double prcntg = Double.parseDouble(((EditText)findViewById(R.id.TipPercentage)).getText().toString());
+        double tip = (total * (prcntg/100) / bills);
+        DecimalFormat nearestCent = new DecimalFormat("#.00");
+        ((TextView) findViewById(R.id.CalculatedTip)).setText(getString(R.string.MonetarySymbol, nearestCent.format(tip)));
     }
 
     public void backButton(View back){
